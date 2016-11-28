19f74dadf2a40763e968fb3c83532ba307d85ca6
s.arendarchuk <ipmazahell@gmail.com>
2016-11-29 00:33:02 +0200

    Stylus improve

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,27 +1,99 @@
 import React, {Component} from 'react';
-import Header from "./_blocks/Header";
+// import Header from "./_blocks/Header";
 
-import LiTable from "./_blocks/oneColumnTable";
+// import LiTable from "./_blocks/oneColumnTable";
 
 class Home extends Component {
 
 	render() {
 		return (
 			<div>
-				<Header />
-				<div className="row">
-					<div className="col-4">
-						<LiTable />
+				<header className="m-b-3">
+					<div id="head">
+						<a href="/">EVE-Prod.</a>
 					</div>
-					<div className="col-4">
-						<LiTable />
-					</div>
-					<div className="col-4">
-						<LiTable />
-					</div>
+				</header>
+				<nav className="blueback m-b-3" id="header-menu">
+					<ul>
+						<li>Calculators</li>
+						<li>Market</li>
+						<li>Item</li>
+					</ul>
+				</nav>
 
+				<div className="row">
+					<div className="col-md-4 col-first">
+						<table>
+							<thead>
+							<tr>
+								<th className="t-a_l">Market</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>
+									<ul>
+										<li>Prices</li>
+										<li>Online orders</li>
+									</ul>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+					<div className="col-md-4 col-midd">
+						<table>
+							<thead>
+							<tr>
+								<th className="t-a_l">Market</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>
+									<ul>
+										<li>Prices</li>
+										<li>Online orders</li>
+									</ul>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+					<div className="col-md-4 col-last">
+						<table>
+							<thead>
+							<tr>
+								<th className="t-a_l">Market</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>
+									<ul>
+										<li>Prices</li>
+										<li>Online orders</li>
+										<li>Online check availabilyty</li>
+									</ul>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
-				{ this.props.children }
+
+				<div className="row">
+					<div className="col-md-12">
+						<div className="panel-content">Market Monitoring</div>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-md-12">
+						dsdsd
+					</div>
+				</div>
+
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {browserHistory, Router} from 'react-router';
 import routes from "./routes";
-import "./static/styles/simple-grid.css";
+import "./static/styles/reset.css";
+import "./static/styles/bootstrap.css";
 import "./static/styles/main.css";
 
 ReactDOM.render(<Router history={browserHistory} routes={routes}/>, document.getElementById("root"));
