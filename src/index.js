23b06bb9e8d5fcf18ba2dab5e1d5fa5a6d184d62
s.arendarchuk <ipmazahell@gmail.com>
2016-11-28 15:39:05 +0200

    Presetting App

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './app/Index';
+import '../node_modules/bulma/css/bulma.css'
+import './static/styles/main.css';
+
+ReactDOM.render(
+	<Index />,
+	document.getElementById('root')
+);
